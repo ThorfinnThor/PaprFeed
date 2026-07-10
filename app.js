e31e3c664c009f46f5775477a2f97f64be7c10d2
ThorfinnThor <pixsatmaps@gmail.com>
2026-07-10 11:42:06 +0200

    v95

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const hiddenKey = "paprfeed:hidden";
 const onboardingKey = "paprfeed:onboarding";
 const controlsKey = "paprfeed:controls";
 const recentSearchesKey = "paprfeed:recent-searches";
-const cacheKeyPrefix = "paprfeed:v94:last-feed";
+const cacheKeyPrefix = "paprfeed:v95:last-feed";
 const localFallbackProxyOrigin = "https://paprfeed.com";
 const pubMedFilterMap = {
   all: "all",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "paprfeed-v94";
+const CACHE_NAME = "paprfeed-v95";
 const APP_FILES = [
   "./",
   "./index.html",
@@ -11,8 +11,8 @@ const APP_FILES = [
   "./topics/",
   "./llms.txt",
   "./manifest.webmanifest",
-  "./assets/icon.svg?v=94",
-  "./assets/icon-512.png?v=94",
+  "./assets/icon.svg?v=95",
+  "./assets/icon-512.png?v=95",
   "./assets/book-logo.svg",
   "./assets/og-paprfeed.png",
 ];
